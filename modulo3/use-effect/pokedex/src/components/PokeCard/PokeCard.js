be5e436ad05ac1f748f934f1110a93faae4d8203
unknown <vallefnd@gmail.com>
2022-02-08 16:55:47 -0300

    use-effect inacabado

---
+++ modulo3/use-effect/pokedex/src/components/PokeCard/PokeCard.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export default function PokeCard(pokemon) {
+    const [pokemon, setPokemon] = useState('')
+
+    getPokemon = pokeName => {
+        axios
+    }
+
+  return (
+    <div>
+      salve
+    </div>
+  );
+}
+
