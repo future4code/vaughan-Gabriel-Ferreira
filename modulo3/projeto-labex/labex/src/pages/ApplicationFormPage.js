a37283abdc68aa14ca3b4a5fd721f3d2264fab7d
unknown <vallefnd@gmail.com>
2022-02-14 16:52:01 -0300

    inicio labex

---
+++ modulo3/projeto-labex/labex/src/pages/ApplicationFormPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ApplicationFormPage() {
+  return <div></div>;
+}
---
+++ modulo3/projeto-labex/labex/src/pages/CreateTripPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateTripPage() {
+  return <div></div>;
+}
