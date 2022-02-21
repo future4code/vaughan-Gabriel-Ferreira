ebb3a33adae61dd46d79b1c5b34a181137f44a49
unknown <vallefnd@gmail.com>
2022-02-20 23:48:54 -0300

    inicio do application form page

--- modulo3/projeto-labex/labex/src/pages/TripsListPage.js
+++ modulo3/projeto-labex/labex/src/pages/TripsListPage.js
@@ -5,8 +5,14 @@ import styled from "styled-components";
 
 const TripCard = styled.div`
   display: flex;
+  flex-direction: column;
   border: 1px solid black;
-  width: 30%;
+`
+const CardsContainer = styled.div`
+  margin: 5% ;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 5%;
 `
 
 export default function TripsListPage() {
@@ -28,22 +34,28 @@ export default function TripsListPage() {
     axios.get('https://us-central1-labenu-apis.cloudfunctions.net/labeX/gabriel-valle-vaughan/trips')
     .then((response) => {
       console.log('Deu certo!', response.data.trips)
-      setAllTrips(response.data)
+      setAllTrips(response.data.trips)
     })
     .catch((err) => {
       console.log(err.response)
     })
   }
 
-  console.log('ALO', allTrips) 
+  console.log('AllTrips é:', allTrips)
 
-   const tripsList = state.allTrips.map((t) => {
+  const newAllTrips = allTrips.map((item) => {
     return (
-      <div>
-        {t.trips.name}
-      </div>
+      <TripCard key={item.name}>
+        <h3>{item.name}</h3>
+        <p>Descrição: {item.description}</p>
+        <p>Planeta: {item.planet}</p>
+        <p>Data: {item.date}</p>
+        <p>Duração: {item.durationInDays} dias</p>
+      </TripCard>
     )
   })
+  
+  
 
   return (
     
@@ -51,8 +63,9 @@ export default function TripsListPage() {
       <h1>TRIPS LIST PAGE</h1>
       <button onClick={goToHomePage}>Home</button>
       <button onClick={goToApplicationFormPage}>Fazer formulário</button>
-      {tripsList}
-
+      <CardsContainer>
+      {newAllTrips}
+      </CardsContainer>
     </div>
   );
 }
