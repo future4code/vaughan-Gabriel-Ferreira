7b1a754f88614caf666cfb23dae5f2c88b24aa45
unknown <vallefnd@gmail.com>
2022-06-01 18:25:32 -0300

    começo da implementação da API

--- modulo6/moviecenter/src/App.js
+++ modulo6/moviecenter/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { Body } from "./components/Body/Body";
 import { Header } from "./components/Header/Header";
 import { SubHeader } from "./components/SubHeader/SubHeader";
--- modulo6/moviecenter/src/components/Body/Body.js
+++ modulo6/moviecenter/src/components/Body/Body.js
@@ -1,7 +1,36 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { key } from "../../constants/key"
+import { BodyContainer } from "./Style"
+
+
+
+
 export const Body = () => {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect (() =>{
+        console.log("montou")
+        getMovies()
+    }, [])
+    
+    const getMovies = () => {
+        axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
+        .then((response) => {
+            console.log("FUNCIONOU!!!!!!!!", response)
+            setMovies(response.data.results)
+            console.log("OLHA AQUI OS FILMES:", movies)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+
     return (
-        <div>
-            SOU O CORPÃO
-        </div>
+        <BodyContainer>
+            CORPA1
+        </BodyContainer>
     )
 }
