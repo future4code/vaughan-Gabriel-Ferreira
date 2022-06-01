1b77ec0eb56b4a12ffb0e1c54693b5569bf42c1e
unknown <vallefnd@gmail.com>
2022-06-01 17:15:14 -0300

    começo do body

--- modulo6/moviecenter/src/App.js
+++ modulo6/moviecenter/src/App.js
@@ -1,3 +1,4 @@
+import { Body } from "./components/Body/Body";
 import { Header } from "./components/Header/Header";
 import { SubHeader } from "./components/SubHeader/SubHeader";
 
@@ -7,7 +8,7 @@ const App = () => {
     <>
       <Header/>
       <SubHeader/>
-      VARIOS FILMES AQ
+      <Body/>
     </>
   );
 }
