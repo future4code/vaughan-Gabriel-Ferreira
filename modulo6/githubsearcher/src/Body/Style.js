9249d5038b91a697cb57797eaca9b83662e2811b
unknown <vallefnd@gmail.com>
2022-06-09 19:27:24 -0300

    pequenas alterações em margin e font

--- modulo6/githubsearcher/src/Body/Style.js
+++ modulo6/githubsearcher/src/Body/Style.js
@@ -15,13 +15,20 @@ export const BodyContainer = styled.div`
 export const SearchBarContainer = styled.div`
 
     margin-top: 10%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     button{
-        height: 56px;
+        height: 40px;
+        width: 20%;
+        margin-top: 10%;
     }
+
     input{
         height: 50px;
         width: 450px;
-
+        font-size: 50px;
     }
 `
