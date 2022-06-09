041f62cf08751d24b7189ae4febfde09ef2e2d92
unknown <vallefnd@gmail.com>
2022-06-09 17:51:20 -0300

    grid de filmes feito

--- modulo6/moviecenter/src/components/Body/Body.js
+++ modulo6/moviecenter/src/components/Body/Body.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import { key } from "../../constants/key"
-import { BodyContainer } from "./Style"
+import { BodyContainer, MovieCard, MovieTitleContainer } from "./Style"
 
 
 
@@ -9,11 +9,25 @@ import { BodyContainer } from "./Style"
 export const Body = () => {
 
     const [movies, setMovies] = useState([])
+    const [images, setImages] = useState([])
 
     useEffect (() =>{
         console.log("montou")
         getMovies()
+        getImages()
     }, [])
+
+    const getImages = () => {
+        axios
+        .get(`https://api.themoviedb.org/3/movie/338953/images?api_key=${key}&language=en-US`)
+        .then((response) => {
+            console.log("AS IMAGENS: ", response)
+            // setImages(response.data.results)
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
     
     const getMovies = () => {
         axios
@@ -21,6 +35,7 @@ export const Body = () => {
         .then((response) => {
             console.log("FUNCIONOU!!!!!!!!", response)
             setMovies(response.data.results)
+            console.log("OLHA AQUI O DATA", response.data)
             console.log("OLHA AQUI OS FILMES:", movies)
         })
         .catch((err) => {
@@ -28,9 +43,23 @@ export const Body = () => {
         })
     }
 
+    const mappedMovies = movies.map((movie) => {
+        return(
+            <MovieCard>
+            <img src="https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289__480.jpg"/>
+            <MovieTitleContainer>
+            {movie.title}
+            </MovieTitleContainer>
+            <div>
+            Lançamento: {movie.release_date}
+            </div>
+            </MovieCard>
+        )
+    })
+
     return (
         <BodyContainer>
-            CORPA1
+            {mappedMovies}
         </BodyContainer>
     )
 }
--- modulo6/moviecenter/src/components/Body/Style.js
+++ modulo6/moviecenter/src/components/Body/Style.js
@@ -2,4 +2,27 @@ import styled from "styled-components"
 
 export const BodyContainer = styled.div`
     display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr ;
+    gap: 70px;
+    img{
+        width: 250px;
+        height: 300px;
+    }
+`
+
+export const MovieCard = styled.div`
+    margin-top: 20px;
+    margin-left: 15%;
+    display: flex;
+    flex-direction: column;
+    div{
+        width: 250px;
+    }
+    font-family: "Roboto", sans-serif;
+    font-weight: bold; 
+`
+
+export const MovieTitleContainer = styled.div`
+    margin-bottom: 7px;
+    margin-top: 5%;
 `
